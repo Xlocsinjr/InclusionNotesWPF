29a3a53d7ee0beaf7aea66f0702d568105ced527
xlocsinjr <x.locsinjr@amolf.nl>
2022-03-16 12:31:40 +0100

    first commit

---
+++ InclusionNotesWPF/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace InclusionNotesWPF;
+
+internal class Program
+{
+	// --------------------------------------------------------------------------------------------------------------------------------
+
+	/// <summary>The main entry point for the application. Set this in project properties -> Application -> Startup Object.</summary>
+	/// <param name="args">The command line arguments passed to the application.</param>
+	[STAThread]
+	private static void Main(string[] args)
+	{
+
+
+		App application = new App();
+		application.InitializeComponent();
+		application.Run();
+	}
+
+	// --------------------------------------------------------------------------------------------------------------------------------
+}
